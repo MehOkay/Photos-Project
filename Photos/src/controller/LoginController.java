427bdd7c28e3dd2a1af601bce60d622c45eafbe4
wcheung530 <wes.cheung530@gmail.com>
2019-11-17 15:23:43 -0500

    project is able to open to login screen

--- Photos/src/controller/LoginController.java
+++ Photos/src/controller/LoginController.java
@@ -1,5 +1,12 @@
 package controller;
 
+import javafx.stage.Stage;
+
 public class LoginController {
+
+	public void start(Stage mainStage) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- Photos/src/photos/Photos.java
+++ Photos/src/photos/Photos.java
@@ -1,10 +1,48 @@
 package photos;
 
-public class Photos {
+import controller.LoginController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+public class Photos extends Application {
+
+	/**
+	 * starts application
+	 * 
+	 * @param mainStage
+	 *            the main stage
+	 */
+	public void start(Stage mainStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/view/login.fxml"));
+			Pane root = (Pane) loader.load();
+			Scene scene = new Scene(root);
+
+			mainStage.setScene(scene);
+			mainStage.setResizable(false);
+			mainStage.setTitle("Photo Album");
+			mainStage.show();
+
+			LoginController controller = loader.getController();
+			controller.start(mainStage);
+
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+	}
+
+	/**
+	 * main method
+	 * 
+	 * @param args
+	 *            command-line arguments
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		launch(args);
 	}
 
 }
